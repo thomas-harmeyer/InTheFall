ee350554cba74f960d79a8d9abfdb559bf3e1c47
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2018-06-13 13:58:00 -0500

    Init in the fall

---
+++ InTheFall/InTheFall/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace InTheFall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Where are you going to be in the fall?");
+            Console.ReadKey();
+        }
+    }
+}
